dca9b60723275396447f225ed1a9c9da5a132201
adriana <atoledosantiago1@gmail.com>
2020-05-12 17:18:57 -0500

    mvp

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,16 @@
-//Complete the necessary code in this file
-// import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+import dummyData from "../../dummy-data";
 
 const PostsPage = () => {
-  // set up state for your data
+  const [instaData] = useState(dummyData);
+
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {instaData.map((post, index) => {
+        return <Post key={index} post={post} />;
+      })}
     </div>
   );
 };
